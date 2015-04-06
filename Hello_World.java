3f61909cf0211a491b44e60d3e2c6bfb2141b730
Xephorium <cac6g3@mst.edu>
2015-04-06 13:43:15 -0500

    Corrected Syntax Errors in Main

--- Hello_World.java
+++ Hello_World.java
@@ -3,8 +3,7 @@ public class Hello_World
 {
   public static void main(String[] args)
   {
-    Purposeless_Class ricky = new Purposeless_Class(ricky, 0);
-    ricky.setName("Ricky");
+    Purposeless_Class ricky = new Purposeless_Class("Ricky", 0);
 
     System.out.println("\nHello World!");
     System.out.print(ricky.getName() + ": " + ricky.getNumBurritos() + " Burritos ");
@@ -14,8 +13,8 @@ public class Hello_World
     	System.out.println(":(");
 
     System.out.println("\nRestocking...");
-    ricky.setBurritos(5);
-    
+    ricky.setNumBurritos(5);
+
     System.out.print(ricky.getName() + ": " + ricky.getNumBurritos() + " Burritos ");
     if(ricky.happy())
     	System.out.println(":D");
