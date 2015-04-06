7de417b860f69675cd7e4ca0ffbd79c8fc9ef0f9
Xephorium <cac6g3@mst.edu>
2015-04-06 13:40:50 -0500

    Added Purposeless_Class Implementation

--- Hello_World.java
+++ Hello_World.java
@@ -3,6 +3,23 @@ public class Hello_World
 {
   public static void main(String[] args)
   {
-    System.out.println("Wazzaaap?!");
+    Purposeless_Class ricky = new Purposeless_Class(ricky, 0);
+    ricky.setName("Ricky");
+
+    System.out.println("\nHello World!");
+    System.out.print(ricky.getName() + ": " + ricky.getNumBurritos() + " Burritos ");
+    if(ricky.happy())
+    	System.out.println(":D");
+    else
+    	System.out.println(":(");
+
+    System.out.println("\nRestocking...");
+    ricky.setBurritos(5);
+    
+    System.out.print(ricky.getName() + ": " + ricky.getNumBurritos() + " Burritos ");
+    if(ricky.happy())
+    	System.out.println(":D");
+    else
+    	System.out.println(":(");
   }
 }
